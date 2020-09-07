99cdde78ac7fcf91f3bfb009810ca3a2bc2b259f
vitorrv10 <57807200+vitorrv10@users.noreply.github.com>
2020-09-07 17:48:35 -0300

    Add files via upload

---
+++ OBI 2017 Frete Nível 2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#define _ ios::sync_with_stdio(false); cout.tie(0); cin.tie(0);
+#define MAX 1123
+#define INF 1123456
+
+using namespace std;
+
+int adj[MAX][MAX];
+int n, m, a, b, c;
+
+void floydWarshall()
+{
+    for (int k = 1; k <= n; ++k)
+    {
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= n; ++j)
+            {
+                adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+            }
+        }
+    }
+}
+
+int main()
+{_
+    cin >> n >> m;
+    for (int i = 0; i <= n; ++i)
+    {
+        for (int j = 0; j <= n; ++j)
+        {
+            if (i != j) adj[i][j] = INF;
+            else adj[i][j] = 0;
+        }
+    }
+    while (m--)
+    {
+        cin >> a >> b >> c;
+        adj[a][b] = c;
+        adj[b][a] = c;
+    }
+    floydWarshall();
+    cout << adj[1][n];
+}
