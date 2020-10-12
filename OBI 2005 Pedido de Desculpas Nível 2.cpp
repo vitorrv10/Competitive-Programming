3d1a428d86f8b3604ae38f8ea6c75452533df944
vitorrv10 <57807200+vitorrv10@users.noreply.github.com>
2020-10-12 16:55:20 -0300

    Add files via upload

---
+++ OBI 2005 Pedido de Desculpas Nível 2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#define _ ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define MAX 112
+#define MAXM 112345
+#define ll long long
+
+using namespace std;
+
+ll N, W, w[MAX], v[MAX], memo[MAX][MAXM];
+
+ll f(ll idx, ll cap){
+    if (idx < 1) return 0;
+    if (cap == 0) return 0;
+    if (memo[idx][cap] != -1) return memo[idx][cap];
+    ll ans = 0;
+    if (w[idx] <= cap) {
+        ans = max(f(idx - 1, cap-w[idx]) + v[idx], f(idx - 1, cap));
+    } else {
+        ans = f(idx - 1, cap);
+    }
+    return memo[idx][cap] = ans;
+}
+
+int main() {_
+    int i = 1;
+    while (true){
+        cin >> W >> N; 
+        if (!W && !N) break;
+        for (int i = 1; i <= N; ++i) {
+            cin >> w[i] >> v[i];      
+        }
+        for (int i = 0; i <= N; ++i) {
+            for (int j = 0; j <= W; ++j) {
+                memo[i][j] = -1;
+            }
+        }
+        cout << "Teste " << i << '\n' << f(N, W) << "\n\n";
+        ++i;
+    }
+}
